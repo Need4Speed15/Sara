75701f42501944387c9de6e1d320677c0247d983
Need4Speed15 <codybrown_151@outlook.com>
2025-06-08 19:15:31 -0400

    saras website v1.0.0

--- index.js
+++ index.js
@@ -1,35 +1,63 @@
 document.addEventListener('DOMContentLoaded', function() {
   const homePhotos = document.querySelector('.home-photos');
+  const homeVideos = document.querySelector('.home-videos');
+  let homeVideosList = '';
   let homePhotosList = '';
-  for (let i = 1; i <= 10; i++) {
+  for (let i = 1; i <= 5; i++) {
     const photo = `<img src="images/home/home${i}.jpg" alt="Home ${i}">`;
     homePhotosList += photo;
   }
+  for (let i = 1; i <= 1; i++) {
+    const video = `<video class="home-video" src="images/home/videos/home${i}.mov" alt="home Video ${i}" controls autoplay loop muted></video>`;
+    homeVideosList += video;
+  }
   if (homePhotos) homePhotos.innerHTML = homePhotosList;
+  if (homeVideos) homeVideos.innerHTML = homeVideosList;
 
   const saraPhotos = document.querySelector('.sara-photos');
+  const saraVideos = document.querySelector('.sara-videos');
+  let saraVideosList = '';
   let saraPhotosList = '';
-  for (let i = 1; i <= 4; i++) {
+  for (let i = 1; i <= 29; i++) {
     const photo = `<img src="images/sara/sara${i}.jpg" alt="Sara ${i}">`;
     saraPhotosList += photo;
   }
+  for (let i = 1; i <= 4; i++) {
+    const video = `<video class="sara-video" src="images/sara/videos/sara${i}.mov" alt="Sara Video ${i}" controls autoplay loop muted></video>`;
+    saraVideosList += video;
+  }
   if (saraPhotos) saraPhotos.innerHTML = saraPhotosList;
+  if (saraVideos) saraVideos.innerHTML = saraVideosList;
 
   const tynsleePhotos = document.querySelector('.tynslee-photos');
+  const tynsleeVideos = document.querySelector('.tyns-videos');
+  let tynsleeVideosList = '';
   let tynsleePhotosList = '';
-  for (let i = 1; i <= 4; i++) {
-    const photo = `<img src="images/tynslee/tynslee${i}.jpg" alt="Tynslee ${i}">`;
+  for (let i = 1; i <= 8; i++) {
+    const photo = `<img src="images/tynslee/tyns${i}.jpg" alt="Tyns ${i}">`;
     tynsleePhotosList += photo;
   }
+  for (let i = 1; i <= 5; i++) {
+    const video = `<video class="tyns-video" src="images/tynslee/videos/tyns${i}.mov" alt="Tyns Video ${i}" controls autoplay loop muted></video>`;
+    tynsleeVideosList += video;
+  }
   if (tynsleePhotos) tynsleePhotos.innerHTML = tynsleePhotosList;
+  if (tynsleeVideos) tynsleeVideos.innerHTML = tynsleeVideosList;
 
   const masonPhotos = document.querySelector('.mason-photos');
+  const masonVideos = document.querySelector('.mason-videos');
+  let masonVideosList = '';
   let masonPhotosList = '';
-  for (let i = 1; i <= 4; i++) {
+  for (let i = 1; i <= 8; i++) {
     const photo = `<img src="images/mason/mason${i}.jpg" alt="Mason ${i}">`;
     masonPhotosList += photo;
   }
+  for (let i = 1; i <= 6; i++) {
+    const video = `<video class="mason-video" src="images/mason/videos/mason${i}.mov" alt="Mason Video ${i}" controls autoplay loop muted></video>`;
+    masonVideosList += video;
+  }
   if (masonPhotos) masonPhotos.innerHTML = masonPhotosList;
+  if (masonVideos) masonVideos.innerHTML = masonVideosList;
 
   const csPhotos = document.querySelector('.cs-photos');
   const csVideos = document.querySelector('.cs-videos');
@@ -39,7 +67,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const photo = `<img class="cs-photo" src="images/cs/cs${i}.jpg" alt="C+S ${i}">`;
     csPhotosList += photo;
   }
-  for (let i = 2; i <= 15; i++) {
+  for (let i = 2; i <= 16; i++) {
     const video = `<video class="cs-video" src="images/cs/videos/cs${i}.mov" alt="C+S Video ${i}" controls autoplay loop muted></video>`;
     csVideosList += video;
   }
